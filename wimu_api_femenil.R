38d779c641c99862573c0fed5cbc0b660c2e288b
Mateo Rodriguez <mr2293@cornell.edu>
2026-03-25 14:51:45 -0600

    Initial commit

---
+++ wimu_api_femenil.R
@@ -0,0 +1,289 @@
+#Libraries
+library(mice) #needed for Power BI
+library(httr) #request (GET) to the API
+library(rjson) #transform JSON to dataframe
+library(vctrs) #join dataframes by rows
+library(lubridate) #transform date column (character) to yyyy-MM-dd (date)
+library(tidyr) #expand lists from dataframe
+library(anytime) #transform data format to UNIX
+library(dplyr) #selecting columns and manipulating dataframes
+Sys.setlocale("LC_CTYPE", "en_US.UTF-8") 
+options(scipen = 100)
+
+#Token (user and password for the WIMU API, plus start date)
+##CHANGE "username" AND "password" BEFORE RUNNING THE SCRIPT
+username <- ifelse(nchar(Sys.getenv("WIMU_USERNAME")) > 0, Sys.getenv("WIMU_USERNAME"), "America")
+password <- ifelse(nchar(Sys.getenv("WIMU_PASSWORD")) > 0, Sys.getenv("WIMU_PASSWORD"), "America@2026")
+date <- "2025/12/17"
+# date_end <- "2026/03/18"
+team <- "5d55d7172ab79c0012d744f7"
+get_token <- GET(paste("https://femexfut.wimucloud.com/apis/rest/login?username=",username,"&password=",password,sep=""))
+get_token <- toJSON(content(get_token))
+get_token <- as.data.frame(jsonlite::fromJSON(get_token))
+
+#Players Endpoint to Dataframe
+players <- data.frame()
+for(i in 1:10) {
+  pla <- GET(paste("https://femexfut.wimucloud.com/apis/rest/players?page=",i,sep=""),
+             add_headers(Authorization=paste(get_token[[1]])))
+  pla <- toJSON(content(pla))
+  pla <- as.data.frame(jsonlite::fromJSON(pla))
+  players <- vec_rbind(players,pla)
+}
+rm(pla)
+
+# players_flat <- players
+# 
+# for (col in names(players_flat)) {
+#   if (is.list(players_flat[[col]])) {
+#     players_flat[[col]] <- sapply(players_flat[[col]], function(x) {
+#       tryCatch(
+#         paste(unlist(x), collapse = ", "),
+#         error = function(e) NA_character_
+#       )
+#     })
+#   }
+# }
+# 
+# write.csv(players_flat, "/Users/mateorodriguez/Desktop/players.csv", row.names = FALSE)
+
+#Teams Endpoint to Dataframe
+teams <- GET("https://femexfut.wimucloud.com/apis/rest/teams",
+             add_headers(Authorization=paste(get_token[[1]])))
+teams <- toJSON(content(teams))
+teams <- as.data.frame(jsonlite::fromJSON(teams))
+
+# Team IDs
+# America Primer Equipo : 5d6812762ab79c0013380530
+# America Femenil : 5d55d7172ab79c0012d744f7
+# America Sub-23 : 5af93feef5acc8125ec33c36
+
+#Attributes Endpoint to Dataframe
+attributes <- GET("https://femexfut.wimucloud.com/apis/rest/attributes",
+                  add_headers(Authorization=paste(get_token[[1]])))
+attributes <- toJSON(content(attributes))
+attributes <- as.data.frame(jsonlite::fromJSON(attributes))
+
+#Loop to extract sesions dataframes based on the page parameter (10 pages => last 2000 sessions)
+sessions <- data.frame()
+for(i in 1:10) {
+  ses <- GET(paste("https://femexfut.wimucloud.com/apis/rest/sessions?sort=start,desc&page=",
+                   i,sep=""),
+             add_headers(Authorization=paste(get_token[[1]])))
+  ses <- toJSON(content(ses))
+  ses <- as.data.frame(jsonlite::fromJSON(ses))
+  sessions <- vec_rbind(sessions,ses)
+}
+rm(ses)
+##Adding columns for date, time and date_time using "start" (UNIX format)
+# sessions$date_time <- as_datetime((sessions$start)/1000) #/1000 to convert milliseconds
+# sessions$date <- substr(sessions$date_time, start = 1, stop = 10)
+# sessions$time <- substr(sessions$date_time, start = 12, stop = 20)
+
+#Loop to extract informs dataframes filtering by the start date
+##If there is no date, we download the last 7 days
+date1 <- ifelse(nchar(date) < 10,today()-7,date)
+date1 <- anydate(date1)
+start <- as.numeric(as.POSIXct(date1))*1000
+# end <- as.numeric(as.POSIXct(date_end))*1000
+team <- team
+##We need to extract the ID of each session from the start date
+sessions_informs <- data.frame()
+for (i in 1:10) {
+  sessions_inf <- GET(paste("https://femexfut.wimucloud.com/apis/rest/sessions?sort=start,desc&page=",
+                            i,"&start=",start,
+                            # "&end=",end,
+                            "&team=",team,sep=""),add_headers(Authorization=paste(get_token[[1]])))
+  sessions_inf <- toJSON(content(sessions_inf))
+  sessions_inf <- as.data.frame(jsonlite::fromJSON(sessions_inf))
+  sessions_informs <- vec_rbind(sessions_informs,sessions_inf)
+}
+# sessions_informs <- as.data.frame(sessions_informs)
+# sessions_informs$date_time <- as_datetime((sessions_informs$start)/1000)
+# sessions_informs$date <- substr(sessions_informs$date_time, start = 1, stop = 10)
+# sessions_informs$time <- substr(sessions_informs$date_time, start = 12, stop = 20)
+##Adding the session ID to the URL that we will use to get the informs data for each session
+urls <- c()
+for (i in 1:nrow(sessions_informs)) {
+  urls[i] <- paste("https://femexfut.wimucloud.com/apis/rest/informs?session=",
+                   sessions_informs$id[[i]],sep = "")
+}
+##Creating a dataframe for each session to finally merge all of them under the same dashboard
+informs <- data.frame() #empty dataframe
+for(i in 1:length(urls)) {
+  inf <- GET(urls[i], add_headers(Authorization=paste(get_token[[1]]))) #API request
+  inf <- toJSON(content(inf)) #extract content and convert to JSON
+  inf <- as.data.frame(jsonlite::fromJSON(inf)) #convert from JSON to dataframe
+  informs <- vec_rbind(informs,inf) #join dataframe extracted with the previous
+}
+rm(sessions_inf) 
+rm(urls) 
+rm(inf) 
+##Date & Time columns
+# informs$date_time <- as_datetime((informs$start)/1000) 
+# informs$date <- substr(informs$date_time, start = 1, stop = 10)
+# informs$time <- substr(informs$date_time, start = 12, stop = 20)
+sessions_informs$date_time <- as_datetime((sessions_informs$start)/1000)
+# sessions_informs$date <- substr(sessions_informs$date_time, start = 1, stop = 10)
+# sessions_informs$time <- substr(sessions_informs$date_time, start = 12, stop = 20)
+
+#Expand columns
+players <- jsonlite::flatten(players)
+sessions <- jsonlite::flatten(sessions)
+informs <- jsonlite::flatten(informs)
+
+#Removing "list" columns from players and sessions (not needed)
+players <- purrr::discard(players, is.list)
+sessions <- purrr::discard(sessions, is.list)
+
+#Columns needed for the output
+##Player: Max Speed, Max Acc, Max Dec
+players_selection <- players[,c("id","wimuName","position","maxSpeed","maxAcc","minAcc")]
+##Teams: Name
+teams_selection <- teams[,c("id","name")]
+##Session: Group, Team, Date, Name, Type
+sessions_selection <- sessions_informs[,c("id","date_time","name","type","center","matchDay",
+                                          "group","weekCalendar")]
+##Attributes: Weather (Session), Contact Level (Drill) 
+attributes_selection <- attributes[,c("id","name","tags")]
+##Informs: Core Metrics
+informs_selection <- informs[,c(
+  "team",
+  "username",
+  "position",
+  "created",
+  "matchDay",
+  "start",
+  "end",
+  "session",
+  "task",
+  "player",
+  "duration",
+  "drillsDuration",
+  "distance.distance",
+  "distance.distanceMin",
+  "distance.HSRRelCount",
+  "distance.HSRRelDistance",
+  "distance.HSRAbsCount",
+  "distance.HSRAbsDistance",
+  "distance.percentageHSRAbs",
+  "distance.ranges",
+  "accelerations.accelerations",
+  # "accelerations.highIntensityAccAbsCounter",
+  # "accelerations.highIntensityAccAbsDistance",
+  "accelerations.decelerations",
+  # "accelerations.highIntensityDecAbsCounter",
+  # "accelerations.highIntensityDecAbsDistance",
+  # "accelerations.maxAcceleration",
+  # "accelerations.maxDeceleration",
+  "accelerations.ranges",
+  "sprint.abs",
+  "sprint.nSprints",
+  "sprint.distance",
+  "sprint.distanceRelative",
+  "sprint.maxSpeed",
+  "sprint.relRepetitions",
+  "load.hmld",
+  "load.hmldMin",
+  "load.Player_Load",
+  "load.Player_LoadMin",
+  "load.highIntWorkAccDist",
+  "steps.stepBalance"
+  # "rpe.RPECentral",
+  # "rpe.wellnessSleep",
+  # "rpe.wellnessDoms",
+  # "rpe.wellnessStress",
+  # "rpe.wellnessMood",
+  # "rpe.wellnessFatigue"
+)]
+
+# informs_selection <- informs_selection |>
+#   filter(task == "Drills")
+
+#Merge dataframes to get the final output
+##Players
+players_selection <- players_selection %>%
+  rename_with(~ paste0("player.", .), .cols = -id)
+informs_players <- merge(informs_selection, players_selection, 
+                         by.x = "player", by.y = "id", all.x = TRUE)
+names(informs_players)
+##Sessions
+sessions_selection <- sessions_selection %>%
+  rename_with(~ paste0("session.", .), .cols = -id)
+informs_sessions <- merge(informs_players, sessions_selection, 
+                          by.x = "session", by.y = "id", all.x = TRUE)
+##Teams
+teams_selection <- teams_selection %>%
+  rename_with(~ paste0("team.", .), .cols = -id)
+informs_teams <- merge(informs_sessions, teams_selection, 
+                       by.x = "session.center", by.y = "id", all.x = TRUE)
+informs_sessions <- informs_teams
+names(informs_sessions)
+
+
+#Final changes
+##Filter only for Drills - we could also maintain it with all the tasks
+informs_sessions <- informs_sessions[informs_sessions$task == "Drills",]
+
+########## -------------- ############## ----------------- ###############
+########## -------------- ############## ----------------- ###############
+########## -------------- ############## ----------------- ###############
+
+##Extract Speed Zones
+my_id_columns <- names(informs_sessions) #original columns except "distance.ranges"
+my_id_columns <- my_id_columns[my_id_columns != "distance.ranges"]
+informs_sessions_unested <- informs_sessions %>%
+  unnest(col = distance.ranges, names_sep = "_")
+informs_sessions_final <- informs_sessions_unested %>%
+  mutate(column_name = paste0("speed_zones_", distance.ranges_min, "_", distance.ranges_max)) %>%
+  pivot_wider(id_cols = all_of(my_id_columns), names_from = column_name,
+              values_from = distance.ranges_value, values_fill = NA, values_fn = mean) %>%
+  select(-starts_with("distance.ranges_"))
+
+##Extract Acceleration Zones
+my_id_columns <- names(informs_sessions_final) #original columns - "accelerations.ranges"
+my_id_columns <- my_id_columns[my_id_columns != "accelerations.ranges"]
+informs_sessions_unested_v2 <- informs_sessions_final %>%
+  unnest(col = accelerations.ranges, names_sep = "_")
+informs_sessions_final_v2 <- informs_sessions_unested_v2 %>%
+  mutate(column_name = paste0("accelerations_zones_", accelerations.ranges_min, "_", 
+                              accelerations.ranges_max)) %>%
+  pivot_wider(id_cols = all_of(my_id_columns), names_from = column_name,
+              values_from = accelerations.ranges_value, values_fill = NA, values_fn = mean) %>%
+  select(-starts_with("accelerations.ranges_"))
+
+##Convert duration to minutes
+final_dataframe <- informs_sessions_final_v2
+final_dataframe$duration_min <- final_dataframe$duration / 60000
+
+##Percentage of Max Speed
+final_dataframe$percentage_maxSpeed <- (final_dataframe$sprint.maxSpeed / 
+                                          final_dataframe$player.maxSpeed)*100
+
+##Relative metrics (HSR Abs & Rel, Sprint Abs & Rel, Acc & HIA, Dec & HID)
+final_dataframe$distance.HSRRelCountMin <- final_dataframe$distance.HSRRelCount / final_dataframe$duration_min
+final_dataframe$distance.HSRAbsCountMin <- final_dataframe$distance.HSRAbsCount / final_dataframe$duration_min
+final_dataframe$distance.HSRRelDistanceMin <- final_dataframe$distance.HSRRelDistance / final_dataframe$duration_min
+final_dataframe$distance.HSRAbsDistanceMin <- final_dataframe$distance.HSRAbsDistance / final_dataframe$duration_min
+final_dataframe$sprint.absMin <- final_dataframe$sprint.abs / final_dataframe$duration_min
+final_dataframe$sprint.nSprintsMin <- final_dataframe$sprint.nSprints / final_dataframe$duration_min
+final_dataframe$sprint.distanceMin <- final_dataframe$sprint.distance / final_dataframe$duration_min
+final_dataframe$sprint.distanceRelativeMin <- final_dataframe$sprint.distanceRelative / final_dataframe$duration_min
+final_dataframe$accelerations.accelerationsMin <- final_dataframe$accelerations.accelerations / final_dataframe$duration_min
+final_dataframe$accelerations.decelerationsMin <- final_dataframe$accelerations.decelerations / final_dataframe$duration_min
+# final_dataframe$accelerations.highIntensityAccAbsCounterMin <- final_dataframe$accelerations.highIntensityAccAbsCounter / final_dataframe$duration_min
+# final_dataframe$accelerations.highIntensityDecAbsCounterMin <- final_dataframe$accelerations.highIntensityDecAbsCounter / final_dataframe$duration_min
+# final_dataframe$accelerations.highIntensityAccAbsDistanceMin <- final_dataframe$accelerations.highIntensityAccAbsDistance / final_dataframe$duration_min
+# final_dataframe$accelerations.highIntensityDecAbsDistanceMin <- final_dataframe$accelerations.highIntensityDecAbsDistance / final_dataframe$duration_min
+
+##Reorder and Remove Columns
+###Add: week calendar, match day
+names(final_dataframe)
+final_dataframe <- final_dataframe %>% 
+  relocate(session, session.name, session.center, team.name, session.date_time, session.weekCalendar,
+           session.type, session.matchDay, session.group, task, player, player.wimuName, 
+           player.position, player.maxSpeed, player.maxAcc, player.minAcc, duration, duration_min) 
+names(final_dataframe)
+
+final_dataframe <- final_dataframe %>%
+  arrange(session.date_time)
---
+++ wimu_process_primer_equipo.R
@@ -0,0 +1,66 @@
+### SCRIPT PARA PROCESAR DATOS WIMU PRIMER EQUIPO (API) -----
+
+library(pacman)
+p_load(tidyverse, ggplot2, readxl,
+       skimr, scales, lubridate, readr, writexl, zoo, openxlsx, tidylog)
+
+options(scipen = 999)
+
+# Fetch data from the API (produces final_dataframe)
+source("wimu_api.R")
+
+# Rename and transform API columns to match downstream format
+data_micro <- final_dataframe %>%
+  select(-player) %>%  # drop player ID — player.wimuName becomes the player column
+  rename(
+    player            = username,
+    task_name         = task,
+    match_day         = session.matchDay,
+    session_duration  = duration_min,
+    distance_m        = distance.distance,
+    dist_over_time    = distance.distanceMin,
+    HSR_abs_dist      = distance.HSRAbsDistance,
+    perc_HSR_abs      = distance.percentageHSRAbs,
+    max_speed         = sprint.maxSpeed,
+    distance_abs      = sprint.abs,
+    sprints_abs_count = sprint.nSprints,
+    sprints_rel_count = sprint.relRepetitions,
+    player_load       = load.Player_Load,
+    HMLD_m            = load.hmld,
+    step_balance      = steps.stepBalance
+  ) %>%
+  mutate(
+    date            = as.Date(session.date_time),
+    drill_duration  = drillsDuration / 60000,  # milliseconds to minutes
+    HSR_over_time   = HSR_abs_dist / session_duration,
+    perc_HSR_abs    = perc_HSR_abs / 100,
+    acc             = rowSums(across(matches("^accelerations_zones_[0-9]")), na.rm = TRUE),
+    decc            = rowSums(across(matches("^accelerations_zones_-")),     na.rm = TRUE),
+    acc_plus_decc   = acc + decc,
+    tiempo_efectivo = drill_duration / session_duration,
+    RPE             = 3.0,
+    TL              = RPE * player_load,
+    match_day       = ifelse(match_day != "MD", gsub(" MD", "", match_day), match_day)
+  ) %>%
+  filter(!player %in% c("www", "Guillermo Ochoa")) %>%
+  distinct(player, date, match_day, .keep_all = TRUE)
+
+# Select output columns
+data_micro <- data_micro %>%
+  select(player, task_name, match_day, date, session_duration, drill_duration, distance_m,
+         dist_over_time, HSR_abs_dist, HSR_over_time, perc_HSR_abs,
+         HMLD_m, distance_abs, sprints_abs_count, max_speed, acc, decc, acc_plus_decc,
+         player_load, sprints_rel_count, step_balance, tiempo_efectivo, RPE, TL) %>%
+  arrange(date)
+
+# Write outputs — paths come from env vars in CI, fall back to local paths
+path_csv  <- Sys.getenv("DASHBOARD_CARGAS_CSV",
+                        unset = "/Users/mateorodriguez/Desktop/analisis_CA/dashboard_cargas/micros/micros_shiny_comb.csv")
+path_xlsx <- Sys.getenv("CARGAS7_XLSX",
+                        unset = "/Users/mateorodriguez/Desktop/analisis_CA/cargas_fisicas_7/data/Sessions_micro01.xlsx")
+
+write_csv(data_micro, path_csv)
+
+if (dir.exists(dirname(path_xlsx))) {
+  write_xlsx(data_micro, path = path_xlsx)
+}
